20d8e1a4f317c50d6586dfc1bda8757110c0fe1b
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-10-29 19:45:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,8 +15,11 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-        if @@cart.@@cart.each do |i| 
-          resp.write  "#{i}\n" 
+        if @@cart.size > 0 
+            @@cart.each do |i| 
+            resp.write  "#{i}\n" 
+        else 
+            resp.write "Your cart is empty"
           end 
      elsif req.path.match(/add/)
       search_term = req.params["q"]

f064427fa4c1310c6c0e5f038c3693feff6ce4f0
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-10-29 19:54:21 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,7 @@ class Application
             resp.write "Your cart is empty"
           end 
      elsif req.path.match(/add/)
-      search_term = req.params["q"]
+      search_term = req.params["item"]
         if @@items.include?(search_term)
          @@cart << search_term
          resp.write "added #{search_term}"

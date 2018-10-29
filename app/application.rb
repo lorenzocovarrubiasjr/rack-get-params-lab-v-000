971e025062a25d4422e3e8ce94984477b4d13fb6
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-10-29 19:49:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -27,7 +27,7 @@ class Application
         if @@items.include?(search_term)
          @@cart << search_term
        else 
-         resp.write "Error!"
+         resp.write "We don't have that item"
        end 
     else
       resp.write "Path Not Found"
